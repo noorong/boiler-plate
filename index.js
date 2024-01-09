b92d14d368b9ac1c6d28b9fa643847deb85d50a9
Jaehun Park <wogns205@gmail.com>
2024-01-10 00:32:41 +0900

    몽고디비연결, 포스트맨 테스트, 노드몬설치, 헤로쿠는 따로 설정해줘야함

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+const { User } = require("./models/User")
+
+//application/X--www-form-urlencoded 를 분석해서 가져옴
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json을 분석해서 가져옴
+app.use(bodyParser.json())
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://oceanwing:abcd1234@cluster0.3skgbwo.mongodb.net/")
-.then(() => console.log("MongoDB Connected"))
+mongoose.connect(config.mongoURI)
+.then(() => console.log("MongoDB Connected!!!"))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Mongo World!')
+  res.send('Mongo World! 싸라해여 연예가 중계이')
 })
+
+
+app.post('/register', async (req, res) => {
+
+  const user = new User(req.body)
+  try {
+    await user.save();
+    return res.status(200).json({success: true})
+  } catch (err) {
+      return res.json({ success: false, err})
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
